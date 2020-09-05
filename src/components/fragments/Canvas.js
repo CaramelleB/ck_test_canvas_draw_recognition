c09e065dabf780daa0cbb6a649bd0fff367981c8
tyesc <escolano.tyler@gmail.com>
2020-09-05 12:54:09 +0200

    feat: add a functional quick & dirty code to recognize sun

--- src/components/fragments/Canvas.js
+++ src/components/fragments/Canvas.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 
+import { encode, guess } from '../../services/utils';
+
 function Canvas () {
   const canvasRef = useRef(null);
   const [context, setContext] = useState(null);
@@ -9,12 +11,17 @@ function Canvas () {
   let end = { x: 0, y: 0 };
   let canvasOffsetLeft = 0;
   let canvasOffsetTop = 0;
+  let res = '';
+  let up = 0;
+  let dataX;
+  let dataY;
 
   useEffect(() => {
     if (canvasRef.current) {
       const renderCtx = canvasRef.current.getContext('2d');
 
       if (renderCtx) {
+        // canvasRef.current.addEventListener('click', cleanup());
         canvasRef.current.addEventListener('mousedown', handleMouseDown);
         canvasRef.current.addEventListener('mouseup', handleMouseUp);
         canvasRef.current.addEventListener('mousemove', handleMouseMove);
@@ -31,6 +38,11 @@ function Canvas () {
   const handleMouseDown = evt => {
     mouseDown = true;
 
+    res += 'd';
+
+    dataX = [start.x];
+    dataY = [start.y];
+
     start = {
       x: evt.clientX - canvasOffsetLeft,
       y: evt.clientY - canvasOffsetTop,
@@ -44,6 +56,19 @@ function Canvas () {
 
   const handleMouseUp = () => {
     mouseDown = false;
+
+    res += encode(dataX, dataY);
+    up += 1;
+
+    if (res === 'dpoint') {
+      res = 'point';
+    } else {
+      res += 'u';
+    }
+
+    guess(res, up);
+
+    res = '';
   };
 
   const handleMouseMove = evt => {
@@ -58,6 +83,9 @@ function Canvas () {
         y: evt.clientY - canvasOffsetTop,
       };
 
+      dataX.push(end.x);
+      dataY.push(end.y);
+
       // Draw our path
       context.beginPath();
       context.moveTo(start.x, start.y);
@@ -70,10 +98,10 @@ function Canvas () {
   };
 
   const cleanup = () => {
-    // res = '';
+    res = '';
     mouseDown = false;
     context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    // up = 0;
+    up = 0;
   };
 
   return (
